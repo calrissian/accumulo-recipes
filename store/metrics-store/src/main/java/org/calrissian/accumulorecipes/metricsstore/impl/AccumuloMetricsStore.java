885a12ffb169ed88c57efb1bce31853403239c7a
Corey Nolet <cnolet@texeltek.com>
2013-04-04 20:37:39 -0400

    Adding apache license header comments to all files

--- store/metrics-store/src/main/java/org/calrissian/accumulorecipes/metricsstore/impl/AccumuloMetricsStore.java
+++ store/metrics-store/src/main/java/org/calrissian/accumulorecipes/metricsstore/impl/AccumuloMetricsStore.java
@@ -1,5 +1,22 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.calrissian.accumulorecipes.metricsstore.impl;
 
+import org.apache.accumulo.core.client.Connector;
 import org.apache.accumulo.core.security.Authorizations;
 import org.calrissian.accumulorecipes.metricsstore.MetricsStore;
 import org.calrissian.accumulorecipes.metricsstore.domain.MetricUnit;
@@ -10,6 +27,12 @@ import java.util.Iterator;
 
 public class AccumuloMetricsStore implements MetricsStore {
 
+    Connector connector;
+
+    public AccumuloMetricsStore(Connector connector) {
+        this.connector = connector;
+    }
+
     @Override
     public Iterator<MetricUnit> query(Date start, Date end, String group, String type, String name, String metricType, Authorizations auths) {
         return null;
@@ -17,6 +40,7 @@ public class AccumuloMetricsStore implements MetricsStore {
 
     @Override
     public void put(Collection<MetricUnit> metrics) {
+
 
     }
 
