d01b32f015af501eda74b19d6f2ffd8e7245029d
eawagner <ceraw79@hotmail.com>
2013-06-17 23:43:14 -0400

    Added unit tests for simple and stats metric service

--- store/metrics-store/src/main/java/org/calrissian/accumulorecipes/metricsstore/ext/StatsMetricStore.java
+++ store/metrics-store/src/main/java/org/calrissian/accumulorecipes/metricsstore/ext/StatsMetricStore.java
@@ -1,10 +1,10 @@
 package org.calrissian.accumulorecipes.metricsstore.ext;
 
+import org.apache.accumulo.core.security.Authorizations;
 import org.calrissian.accumulorecipes.metricsstore.MetricStore;
 import org.calrissian.accumulorecipes.metricsstore.domain.MetricTimeUnit;
-import org.calrissian.accumulorecipes.metricsstore.ext.domain.MetricStats;
+import org.calrissian.accumulorecipes.metricsstore.ext.domain.Stats;
 
-import java.util.Collection;
 import java.util.Date;
 
 public interface StatsMetricStore extends MetricStore{
@@ -20,7 +20,7 @@ public interface StatsMetricStore extends MetricStore{
      * @param auths
      * @return
      */
-    Iterable<MetricStats> queryStats(Date start, Date end, String group, String type, String name,
-                           MetricTimeUnit timeUnit, Collection<String> auths);
+    Iterable<Stats> queryStats(Date start, Date end, String group, String type, String name,
+                           MetricTimeUnit timeUnit, Authorizations auths);
 
 }
---
+++ store/metrics-store/src/test/java/org/calrissian/accumulorecipes/metricsstore/impl/AccumuloMetricStoreTest.java
@@ -0,0 +1,102 @@
+package org.calrissian.accumulorecipes.metricsstore.impl;
+
+
+import com.google.common.collect.AbstractIterator;
+import org.apache.accumulo.core.client.AccumuloException;
+import org.apache.accumulo.core.client.AccumuloSecurityException;
+import org.apache.accumulo.core.client.Connector;
+import org.apache.accumulo.core.client.mock.MockInstance;
+import org.apache.accumulo.core.security.Authorizations;
+import org.calrissian.accumulorecipes.metricsstore.domain.Metric;
+import org.calrissian.accumulorecipes.metricsstore.domain.MetricTimeUnit;
+import org.junit.Test;
+
+import java.util.Date;
+import java.util.Iterator;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import static com.google.common.collect.Iterables.limit;
+import static com.google.common.collect.Lists.newArrayList;
+import static org.junit.Assert.assertEquals;
+
+public class AccumuloMetricStoreTest {
+
+    public static Connector getConnector() throws AccumuloSecurityException, AccumuloException {
+        return new MockInstance().getConnector("root", "".getBytes());
+    }
+
+    public static long getTimeOffset(MetricTimeUnit timeUnit){
+        switch (timeUnit) {
+            case MINUTES: return TimeUnit.MINUTES.toMillis(1);
+            case HOURS: return TimeUnit.HOURS.toMillis(1);
+            case DAYS: return TimeUnit.DAYS.toMillis(1);
+            case MONTHS: return TimeUnit.DAYS.toMillis(31);
+        }
+        return TimeUnit.MINUTES.toMillis(1);
+    }
+
+    public static Iterable<Metric> generateTestData(MetricTimeUnit timeUnit, int limit) {
+        final long startTime = System.currentTimeMillis();
+        final long offset = getTimeOffset(timeUnit);
+
+        return limit(
+                new Iterable<Metric>() {
+                    @Override
+                    public Iterator<Metric> iterator() {
+                        return new AbstractIterator<Metric>() {
+                            long current = startTime;
+
+                            @Override
+                            protected Metric computeNext() {
+                                current -= offset;
+                                return new Metric(current, "group", "type", "name", "", 1);
+                            }
+                        };
+                    }
+                },
+                limit);
+    }
+
+    public static void checkMetrics(Iterable<Metric> actual, int expectedNum, int expectedVal) {
+        List<Metric> actualList = newArrayList(actual);
+
+        assertEquals(expectedNum, actualList.size());
+
+        for (Metric metric : actualList) {
+            assertEquals("group", metric.getGroup());
+            assertEquals("type", metric.getType());
+            assertEquals("name", metric.getName());
+            assertEquals("", metric.getVisibility());
+            assertEquals(expectedVal, metric.getValue());
+        }
+    }
+
+    @Test
+    public void testStoreAndQuery() throws Exception {
+        AccumuloMetricStore metricStore = new AccumuloMetricStore(getConnector());
+
+        Iterable<Metric> testData = generateTestData(MetricTimeUnit.MINUTES, 60);
+
+        metricStore.save(testData);
+
+        Iterable<Metric> actual = metricStore.query(new Date(0), new Date(), "group", "type", "name", MetricTimeUnit.MINUTES, new Authorizations());
+
+        checkMetrics(actual, 60, 1);
+    }
+
+    @Test
+    public void testQueryAggregation() throws Exception {
+        AccumuloMetricStore metricStore = new AccumuloMetricStore(getConnector());
+
+        Iterable<Metric> testData = generateTestData(MetricTimeUnit.MINUTES, 60);
+
+        metricStore.save(testData);
+        metricStore.save(testData);
+        metricStore.save(testData);
+
+        Iterable<Metric> actual = metricStore.query(new Date(0), new Date(), "group", "type", "name", MetricTimeUnit.MINUTES, new Authorizations());
+
+        checkMetrics(actual, 60, 3);
+    }
+}
