16524162d8b1e27928dbcbfe2a74916347e8582d
eawagner <ceraw79@hotmail.com>
2013-06-18 03:43:26 -0400

    Fixed some javadocs for metric service

--- store/metrics-store/src/main/java/org/calrissian/accumulorecipes/metricsstore/ext/custom/function/MinFunction.java
+++ store/metrics-store/src/main/java/org/calrissian/accumulorecipes/metricsstore/ext/custom/function/MinFunction.java
@@ -8,26 +8,42 @@ import static java.lang.Math.min;
  * Custom metric function that calculates the min value.
  */
 public class MinFunction implements MetricFunction<Long> {
+
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public Long intitialValue() {
         return Long.MAX_VALUE;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public Long update(Long orig, long value) {
         return min(orig, value);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public Long merge(Long orig, Long value) {
         return min(orig, value);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public String serialize(Long value) {
         return Long.toString(value);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public Long deserialize(String data) {
         return parseLong(data);
