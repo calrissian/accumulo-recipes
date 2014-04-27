f679d05f0a82fe1bc8a644a9193cebe23da270fd
Corey J. Nolet <cjnolet@gmail.com>
2014-04-27 00:43:24 -0400

    Removing unused guava transform function from merge join iterable

--- store/temporal-last-n-store/src/main/java/org/calrissian/accumulorecipes/temporal/lastn/support/StoreEntryMergeJoinIterable.java
+++ store/temporal-last-n-store/src/main/java/org/calrissian/accumulorecipes/temporal/lastn/support/StoreEntryMergeJoinIterable.java
@@ -1,28 +1,16 @@
 package org.calrissian.accumulorecipes.temporal.lastn.support;
 
-import com.google.common.base.Function;
-import org.apache.accumulo.core.data.Key;
-import org.apache.accumulo.core.data.Value;
 import org.calrissian.accumulorecipes.commons.domain.StoreEntry;
-import org.calrissian.accumulorecipes.temporal.lastn.impl.AccumuloTemporalLastNStore;
-import org.calrissian.mango.collect.CloseableIterable;
 import org.calrissian.mango.collect.PeekingCloseableIterator;
-import org.calrissian.mango.domain.Tuple;
-import org.calrissian.mango.types.TypeRegistry;
 
-import java.io.IOException;
-import java.util.*;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
 
-import static org.apache.commons.lang.StringUtils.splitPreserveAllTokens;
-import static org.calrissian.accumulorecipes.temporal.lastn.impl.AccumuloTemporalLastNStore.DELIM;
-import static org.calrissian.accumulorecipes.temporal.lastn.iterators.EventGroupingIterator.decodeRow;
-import static org.calrissian.mango.accumulo.types.AccumuloTypeEncoders.ACCUMULO_TYPES;
 import static org.calrissian.mango.collect.CloseableIterators.peekingIterator;
 import static org.calrissian.mango.collect.CloseableIterators.wrap;
 
 public class StoreEntryMergeJoinIterable implements Iterable<StoreEntry> {
-
-    private TypeRegistry registry = ACCUMULO_TYPES;
 
     private Iterable<Iterable<StoreEntry>> cursors;
 
@@ -70,25 +58,4 @@ public class StoreEntryMergeJoinIterable implements Iterable<StoreEntry> {
         };
 
     }
-
-    Function<List<Map.Entry<Key,Value>>, StoreEntry> entryXform = new Function<List<Map.Entry<Key, Value>>, StoreEntry>() {
-        @Override
-        public StoreEntry apply(List<Map.Entry<Key, Value>> entries) {
-            StoreEntry toReturn = null;
-            try {
-                for(Map.Entry<Key,Value> tupleCol : entries) {
-                    String[] splits = splitPreserveAllTokens(new String(tupleCol.getValue().get()), DELIM);
-                    if(toReturn == null) {
-                        toReturn = new StoreEntry(splits[0], Long.parseLong(splits[1]));
-                    }
-                    toReturn.put(new Tuple(splits[2], registry.decode(splits[3], splits[4]), splits[5]));
-                }
-            } catch (Exception e) {
-                throw new RuntimeException(e);
-            }
-
-            return toReturn;
-       }
-    };
-
 }
