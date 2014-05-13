eb2f59cfbbfa413f008c3111eec10a49e74e3c89
Corey J. Nolet <cjnolet@gmail.com>
2014-05-12 21:20:57 -0400

    Fixing poorly named argument names for some of the Graph methods

--- store/graph-store/src/main/java/org/calrissian/accumulorecipes/graphstore/tinkerpop/query/EntityGraphQuery.java
+++ store/graph-store/src/main/java/org/calrissian/accumulorecipes/graphstore/tinkerpop/query/EntityGraphQuery.java
@@ -40,43 +40,43 @@ public class EntityGraphQuery implements GraphQuery {
   }
 
   @Override
-  public GraphQuery has(String s) {
-    queryBuilder = queryBuilder.has(s);
+  public GraphQuery has(String key) {
+    queryBuilder = queryBuilder.has(key);
     return this;
   }
 
   @Override
-  public GraphQuery hasNot(String s) {
-    filters = filters.hasNot(s);
+  public GraphQuery hasNot(String key) {
+    filters = filters.hasNot(key);
     return this;
   }
 
   @Override
-  public GraphQuery has(String s, Object o) {
-    queryBuilder = queryBuilder.eq(s, o);
+  public GraphQuery has(String key, Object value) {
+    queryBuilder = queryBuilder.eq(key, value);
     return this;
   }
 
   @Override
-  public GraphQuery hasNot(String s, Object o) {
-    queryBuilder = queryBuilder.notEq(s, o);
+  public GraphQuery hasNot(String key, Object value) {
+    queryBuilder = queryBuilder.notEq(key, value);
     return this;
   }
 
   @Override
-  public GraphQuery has(String s, Predicate predicate, Object o) {
+  public GraphQuery has(String key, Predicate predicate, Object value) {
     if(predicate == EQUAL)
-      return has(s, o);
+      return has(key, value);
     else if(predicate == NOT_EQUAL)
-      return hasNot(s, o);
+      return hasNot(key, value);
     else if(predicate == GREATER_THAN)
-      queryBuilder = queryBuilder.greaterThan(s, o);
+      queryBuilder = queryBuilder.greaterThan(key, value);
     else if(predicate == LESS_THAN)
-      queryBuilder = queryBuilder.lessThan(s, o);
+      queryBuilder = queryBuilder.lessThan(key, value);
     else if(predicate == GREATER_THAN_EQUAL)
-      queryBuilder = queryBuilder.greaterThanEq(s, o);
+      queryBuilder = queryBuilder.greaterThanEq(key, value);
     else if(predicate == LESS_THAN_EQUAL)
-      queryBuilder = queryBuilder.lessThanEq(s, o);
+      queryBuilder = queryBuilder.lessThanEq(key, value);
     else
       throw new UnsupportedOperationException("Predicate with type " + predicate + " is not supported.");
 
@@ -84,13 +84,13 @@ public class EntityGraphQuery implements GraphQuery {
   }
 
   @Override
-  public <T extends Comparable<T>> GraphQuery has(String s, T t, Compare compare) {
-    return has(s, compare, t);
+  public <T extends Comparable<T>> GraphQuery has(String key, T value, Compare compare) {
+    return has(key, compare, value);
   }
 
   @Override
-  public <T extends Comparable<?>> GraphQuery interval(String s, T t, T t2) {
-    queryBuilder = queryBuilder.range(s, t, t2);
+  public <T extends Comparable<?>> GraphQuery interval(String key, T start, T stop) {
+    queryBuilder = queryBuilder.range(key, start, stop);
     return this;
   }
 
