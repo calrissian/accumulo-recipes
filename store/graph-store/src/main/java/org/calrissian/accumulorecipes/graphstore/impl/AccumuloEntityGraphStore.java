c77697e70c15c84351fcd354e68c08df7bfcf8fe
Corey J. Nolet <cjnolet@gmail.com>
2014-05-26 10:27:34 -0400

    Cleaning things up

--- store/graph-store/src/main/java/org/calrissian/accumulorecipes/graphstore/impl/AccumuloEntityGraphStore.java
+++ store/graph-store/src/main/java/org/calrissian/accumulorecipes/graphstore/impl/AccumuloEntityGraphStore.java
@@ -28,7 +28,6 @@ import org.calrissian.accumulorecipes.commons.domain.Auths;
 import org.calrissian.accumulorecipes.commons.domain.StoreConfig;
 import org.calrissian.accumulorecipes.entitystore.impl.AccumuloEntityStore;
 import org.calrissian.accumulorecipes.entitystore.model.EntityIndex;
-import org.calrissian.accumulorecipes.entitystore.model.EntityRelationship;
 import org.calrissian.accumulorecipes.graphstore.GraphStore;
 import org.calrissian.accumulorecipes.graphstore.model.Direction;
 import org.calrissian.accumulorecipes.graphstore.model.EdgeEntity;
@@ -37,9 +36,10 @@ import org.calrissian.accumulorecipes.graphstore.support.TupleStoreCriteriaPredi
 import org.calrissian.accumulorecipes.graphstore.tinkerpop.EntityGraph;
 import org.calrissian.mango.collect.CloseableIterable;
 import org.calrissian.mango.criteria.domain.Node;
-import org.calrissian.mango.domain.entity.Entity;
-import org.calrissian.mango.domain.entity.BaseEntity;
 import org.calrissian.mango.domain.Tuple;
+import org.calrissian.mango.domain.entity.BaseEntity;
+import org.calrissian.mango.domain.entity.Entity;
+import org.calrissian.mango.domain.entity.EntityRelationship;
 
 import java.util.*;
 
@@ -50,12 +50,12 @@ import static org.apache.commons.lang.StringUtils.splitPreserveAllTokens;
 import static org.calrissian.accumulorecipes.commons.support.Constants.DELIM;
 import static org.calrissian.accumulorecipes.commons.support.Constants.EMPTY_VALUE;
 import static org.calrissian.accumulorecipes.commons.support.Scanners.closeableIterable;
-import static org.calrissian.accumulorecipes.entitystore.model.RelationshipTypeEncoder.ALIAS;
 import static org.calrissian.accumulorecipes.graphstore.model.Direction.IN;
 import static org.calrissian.accumulorecipes.graphstore.model.Direction.OUT;
 import static org.calrissian.accumulorecipes.graphstore.model.EdgeEntity.*;
 import static org.calrissian.mango.collect.CloseableIterables.*;
 import static org.calrissian.mango.criteria.support.NodeUtils.criteriaFromNode;
+import static org.calrissian.mango.types.encoders.EntityRelationshipEncoder.ALIAS;
 
 /**
  * The AccumuloEntityGraphStore wraps an {@link AccumuloEntityStore} to provide an extra index which is capable
--- store/graph-store/src/main/java/org/calrissian/accumulorecipes/graphstore/tinkerpop/model/EntityEdge.java
+++ store/graph-store/src/main/java/org/calrissian/accumulorecipes/graphstore/tinkerpop/model/EntityEdge.java
@@ -21,11 +21,11 @@ import com.tinkerpop.blueprints.Edge;
 import com.tinkerpop.blueprints.Vertex;
 import org.calrissian.accumulorecipes.commons.domain.Auths;
 import org.calrissian.accumulorecipes.entitystore.model.EntityIndex;
-import org.calrissian.accumulorecipes.entitystore.model.EntityRelationship;
 import org.calrissian.accumulorecipes.graphstore.GraphStore;
 import org.calrissian.accumulorecipes.graphstore.model.EdgeEntity;
 import org.calrissian.mango.collect.CloseableIterable;
 import org.calrissian.mango.domain.entity.Entity;
+import org.calrissian.mango.domain.entity.EntityRelationship;
 
 import java.util.Iterator;
 
