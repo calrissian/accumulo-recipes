f1b8f980bef4a1c82daafe86b7272188ed9ea8a9
Corey J. Nolet <cjnolet@gmail.com>
2014-06-09 20:29:42 -0400

    Adding test for metadata serde

---
+++ commons/src/test/java/org/calrissian/accumulorecipes/commons/support/metadata/BaseMetadataSerDeTest.java
@@ -0,0 +1,36 @@
+package org.calrissian.accumulorecipes.commons.support.metadata;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.calrissian.mango.types.SimpleTypeEncoders.SIMPLE_TYPES;
+import static org.junit.Assert.assertEquals;
+
+public class BaseMetadataSerDeTest {
+
+    MetadataSerDe metadataSerDe = new BaseMetadataSerDe();
+
+    @Before
+    public void setup() {
+        metadataSerDe.setTypeRegistry(SIMPLE_TYPES);
+    }
+
+    @Test
+    public void testSimpleSerializationDeserialization() {
+
+        Map<String, Object> map = new HashMap<String, Object>();
+        map.put("key1", "val1");
+        map.put("key2", 5);
+        map.put("key3", 10l);
+        map.put("key4", 1.0);
+        map.put("key5", true);
+
+        byte[] bytes = metadataSerDe.serialize(map);
+
+        Map<String,Object> actualMap = metadataSerDe.deserialize(bytes);
+        assertEquals(map, actualMap);
+    }
+}
