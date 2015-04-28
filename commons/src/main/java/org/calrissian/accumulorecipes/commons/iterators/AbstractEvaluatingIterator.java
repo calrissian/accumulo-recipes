78207fbf1af31d86ce57b54a15e84675595bfb27
Corey J. Nolet <cjnolet@gmail.com>
2015-04-28 00:08:41 -0400

    For #331 Fixing the O(n^2) to be O(n), we can do better than this later.

--- commons/src/main/java/org/calrissian/accumulorecipes/commons/iterators/AbstractEvaluatingIterator.java
+++ commons/src/main/java/org/calrissian/accumulorecipes/commons/iterators/AbstractEvaluatingIterator.java
@@ -16,14 +16,15 @@
  */
 package org.calrissian.accumulorecipes.commons.iterators;
 
+import static java.util.Arrays.copyOfRange;
+import static org.apache.commons.lang.StringUtils.join;
 import static org.apache.commons.lang.StringUtils.splitPreserveAllTokens;
+import static org.calrissian.accumulorecipes.commons.iterators.support.EventFields.initializeKryo;
 import static org.calrissian.accumulorecipes.commons.support.Constants.NULL_BYTE;
 import java.io.IOException;
 import java.nio.ByteBuffer;
-import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
@@ -40,7 +41,6 @@ import org.apache.accumulo.core.iterators.IteratorEnvironment;
 import org.apache.accumulo.core.iterators.OptionDescriber;
 import org.apache.accumulo.core.iterators.SortedKeyValueIterator;
 import org.apache.commons.jexl2.parser.ParseException;
-import org.apache.commons.lang.StringUtils;
 import org.apache.log4j.Logger;
 import org.calrissian.accumulorecipes.commons.iterators.support.EventFields;
 import org.calrissian.accumulorecipes.commons.iterators.support.QueryEvaluator;
@@ -79,10 +79,10 @@ public abstract class AbstractEvaluatingIterator implements SortedKeyValueIterat
     private Set<String> skipExpressions = null;
 
     protected static final String SELECT_FIELDS = "selectFields";
-    private Set<String> selectFields;
+    protected Set<String> selectFields;
 
     public static void setSelectFields(IteratorSetting is, Set<String> selectFields) {
-        is.addOption(SELECT_FIELDS, StringUtils.join(selectFields, NULL_BYTE));
+        is.addOption(SELECT_FIELDS, join(selectFields, NULL_BYTE));
     }
 
     protected AbstractEvaluatingIterator(AbstractEvaluatingIterator other, IteratorEnvironment env) {
@@ -198,17 +198,16 @@ public abstract class AbstractEvaluatingIterator implements SortedKeyValueIterat
                         // Wrap in ByteBuffer to work with Kryo
                         ByteBuffer buf = ByteBuffer.wrap(serializedMap);
                         // Serialize the EventFields object
-                        if (selectFields != null) {
-                            Set<String> keys = new HashSet<String>(event.keySet());
-                            for (String key : keys) {
-                                if (!selectFields.contains(key))
-                                    event.removeAll(key);
-                            }
+
+                        EventFields newEventFields = new EventFields();
+                        for(Map.Entry<String,EventFields.FieldValue> entry  : event.entries()) {
+                            if((selectFields != null && selectFields.contains(entry.getKey()) || selectFields == null))
+                                newEventFields.put(entry.getKey(), entry.getValue());
                         }
 
-                        event.write(kryo, new ByteBufferOutput(buf), event);
+                        newEventFields.write(kryo, new ByteBufferOutput(buf), newEventFields);
                         // Truncate array to the used size.
-                        returnValue = new Value(Arrays.copyOfRange(serializedMap, 0, buf.position()));
+                        returnValue = new Value(copyOfRange(serializedMap, 0, buf.position()));
                     } else {
                         returnKey = null;
                         returnValue = null;
@@ -309,7 +308,7 @@ public abstract class AbstractEvaluatingIterator implements SortedKeyValueIterat
         } catch (ParseException e) {
             throw new IllegalArgumentException("Failed to parse criteria", e);
         }
-        EventFields.initializeKryo(kryo);
+        initializeKryo(kryo);
     }
 
     public IteratorOptions describeOptions() {
